5907567242c4778421f39f87e42159c17cd9be85
小磯良稀 <koisoyoshiki@koisoryoumarenoMacBook-Air.local>
2021-05-28 17:48:43 +0900

    [START]javascript

---
+++ javascript/chapter2/app.js
@@ -0,0 +1,16 @@
+// 確認問題
+
+var number1 = 9;
+var number2 = 1;
+
+// 足し算
+alert("9 + 1 = " + (number1 + number2));
+
+// 引き算
+alert("9 - 1 = " + (number1 - number2));
+
+// 掛け算
+alert("9 * 1 = " + (number1 * number2));
+
+// 割り算
+alert("9 / 1 = " + (number1 / number2));
