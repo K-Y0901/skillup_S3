f7c4c33e5164b1ccdc7064a9020d61fdf64acec3
EC2 Default User <yoshiki100901@icloud.com>
2021-06-08 06:17:23 +0000

    [7-a]create GroupUser model

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_085836) do
+ActiveRecord::Schema.define(version: 2021_06_08_061445) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "book_id"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2021_06_04_085836) do
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_favorites_on_book_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.string "user_id"
+    t.string "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "impressions", force: :cascade do |t|
