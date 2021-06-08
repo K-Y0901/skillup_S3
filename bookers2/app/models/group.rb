68f6f2c2ed7560489df54c4623ecb8184f90f310
EC2 Default User <yoshiki100901@icloud.com>
2021-06-08 08:06:10 +0000

    [7-a]finish

--- bookers2/app/models/group.rb
+++ bookers2/app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ApplicationRecord
     has_many :group_users, dependent: :destroy
+    
+    validates :name, presence: true
+    validates :introduction, length: {maximum: 50}
+  
+    attachment :image, destroy: false
 end
--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_061802) do
     t.string "name"
     t.text "introduction"
     t.string "image_id"
-    t.integer "owner"
+    t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
