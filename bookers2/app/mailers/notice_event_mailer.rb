e24b0cc5cc01eff7e526a5f21190051acc0549cd
EC2 Default User <yoshiki100901@icloud.com>
2021-06-10 18:00:05 +0000

    [9-c]create mailer

---
+++ bookers2/app/mailers/notice_event_mailer.rb
@@ -0,0 +1,2 @@
+class NoticeEventMailer < ApplicationMailer
+end
---
+++ bookers2/test/mailers/previews/notice_event_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notice_event_mailer
+class NoticeEventMailerPreview < ActionMailer::Preview
+
+end
