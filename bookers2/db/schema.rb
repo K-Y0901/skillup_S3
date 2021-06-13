eeb0db3defd0b9814f6b818a6029a8a475557c3e
EC2 Default User <yoshiki100901@icloud.com>
2021-06-13 08:47:59 +0000

    [7-d]add rate column

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_171600) do
+ActiveRecord::Schema.define(version: 2021_06_13_082506) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "book_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_171600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "rate"
   end
 
   create_table "entries", force: :cascade do |t|
