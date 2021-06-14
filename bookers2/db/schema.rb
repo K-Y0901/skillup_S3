7b908d2a9cefb1140c7044760c4bd95d5329f7a2
EC2 Default User <yoshiki100901@icloud.com>
2021-06-14 08:26:03 +0000

    [9-d]add category column

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_082506) do
+ActiveRecord::Schema.define(version: 2021_06_14_082515) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "book_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_082506) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.float "rate"
+    t.string "category"
   end
 
   create_table "entries", force: :cascade do |t|
